de7d2bbb5886a0552da44558462e7d7cb7685730
Cheng-Zhen Yang <chengzhenyang@gmail.com>
2020-07-19 19:32:28 +1200

    Test Auto Deployment

---
+++ StudentSIMS/Migrations/20200719072804_AddTimeCreatedAddressModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudentSIMS.Migrations
+{
+    public partial class AddTimeCreatedAddressModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "timeCreated",
+                table: "Address",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "timeCreated",
+                table: "Address");
+        }
+    }
+}
--- StudentSIMS/Models/Address.cs
+++ StudentSIMS/Models/Address.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace StudentSIMS.Models
@@ -8,25 +9,20 @@ namespace StudentSIMS.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int addressId { get; set; }
-
         [ForeignKey("Student")]
         public int studentId { get; set; }
-
         [Required]
         public string streetNumber { get; set; }
-
         [Required]
         public string street { get; set; }
-
         public string suburb { get; set; }
-
         [Required]
         public string city { get; set; }
-
         [Required]
         public string postcode { get; set; }
-
         [Required]
         public string country { get; set; }
+        [Timestamp]
+        public DateTime timeCreated { get; set; }
     }
 }
